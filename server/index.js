2961ee00515c4ca2f766f40988362426278e1133
Daniel Pagan <deapagan@gmail.com>
2022-02-27 13:50:56 -0500

    Setting up simple server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('static'));
+
+app.listen(port, () => {
+    console.log(`Started at http://localhost:${port}/`);
+});
