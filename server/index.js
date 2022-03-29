cb9013e87bce34f168e962b5471022de62d8af24
Daniel Pagan <deapagan@gmail.com>
2022-03-28 22:06:25 -0400

    Dumping fingerprints to file

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
 
+import fs from 'fs';
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.static('static'));
 
+// DB
+const dbFile = './fingerprints.txt';
+
+const storeDB = data => {
+    fs.appendFileSync(dbFile, JSON.stringify(data) + '\n');
+};
+
 app.post('/fingerprint', (req, res) => {
-    // TODO: db.store(req.body["fingerprint"]);
+    console.log("hello")
+    let fp = req.body.fingerprint;
+    console.log("Got fingerprint " + fp);
+    storeDB(fp);
     res.send();
 });
 
